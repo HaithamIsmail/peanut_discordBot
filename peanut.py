6b4e0b63be486b0dc398409c11428b8553592767
haitham ismail <haitham.ismail@eigenctec.de>
2022-07-31 11:14:51 +0300

    Added image command

--- peanut.py
+++ peanut.py
@@ -163,6 +163,7 @@ async def image_command(ctx: SlashContext, *, query:str):
     attribution = photo.get_attribution(format='text')
     link = photo.link_download
     embed = discord.Embed(title="Image Picked:", description=f"**Attribution :** \n{attribution}", color=discord.Color.green())
-    await ctx.send(content = link, embed=embed)
+    await ctx.send(embed=embed)
+    await ctx.send(link)
                 
 bot.run(getenv('TOKEN'))
